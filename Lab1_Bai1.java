550e79115724ddcd194c3fbad6884e3dbfa7837c
VennSu <55218244+VennSu@users.noreply.github.com>
2019-09-30 08:11:39 +0700

    Add files via upload

---
+++ Lab1_Bai1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab1;
+
+import static java.awt.Color.blue;
+import static java.awt.Color.red;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class Lab1_Bai1 extends JFrame{
+    public static void main(String[] args) {
+        new Lab1_Bai1();
+    }
+    public Lab1_Bai1(){
+         setVisible(true);
+        setTitle("Nguyen dinh tin");
+        setBackground(blue);
+        setForeground(red);
+        setSize(400,400);
+    }
+}
